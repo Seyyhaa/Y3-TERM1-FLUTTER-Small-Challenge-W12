431ab629cd1f2f34e62796dc3d6b467b7a0610cb
Seyha Kruy <seyhakruy168168@gmail.com>
2025-12-16 23:05:26 +0700

    Small Challenge'

---
+++ ui/groceries/grocery_list.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/ui/groceries/grocery_form.dart';
+import '../../data/mock_grocery_repository.dart';
+import '../../models/grocery.dart';
+
+class GroceryList extends StatefulWidget {
+  
+  const GroceryList({super.key, });
+ 
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<Grocery> groceries = [];
+  void onCreate() async {
+    //Navigate to the form screen using the Navigator push
+    final Grocery? newItem = await Navigator.push(
+    context,
+    MaterialPageRoute (
+      builder: (context) => const NewItem() ),
+    );  
+
+    if(newItem != null){
+    setState((){
+      groceries.add(newItem);
+    }
+    );
+  }
+  
+
+  
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+     
+    final allGrocery = [...dummyGroceryItems,...groceries];
+    Widget content = const Center(child: Text('No items added yet.'));
+
+    if (allGrocery.isNotEmpty) {
+      //  1 - Display groceries with an Item builder and  LIst Tile
+      content = ListView.builder(
+        itemCount: allGrocery.length,
+        itemBuilder: (context, index) =>
+            GroceryTile(grocery: allGrocery[index]),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: onCreate, icon: const Icon(Icons.add))],
+      ),
+      body: content,
+    );
+  }
+}
+
+class GroceryTile extends StatelessWidget {
+  const GroceryTile({super.key, required this.grocery});
+
+  final Grocery grocery;
+
+  @override
+  Widget build(BuildContext context) {
+    //  2 - Display groceries with an Item builder and  LIst Tile
+    return  ListTile(
+      leading: Container(width: 15, height: 15, color:grocery.category.color,),
+      title: Text(grocery.name),
+      trailing: Text(grocery.quantity.toString()),
+    );
+  }
+
+}
